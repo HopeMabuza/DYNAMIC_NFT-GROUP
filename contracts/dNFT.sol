426fd22844cc27f7ce21dac4e6df8b5587ddda68
HopeMabuza <londiwemabuza94@gmail.com>
2026-04-21 18:27:10 +0200

    added soulbound and exclusive minting feature

--- contracts/dNFT.sol
+++ contracts/dNFT.sol
@@ -2,63 +2,70 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin/contracts/access/Ownable.sol"
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract dNFT is ERC721, Ownable{
-
+contract dNFT is ERC721, Ownable {
     uint256 private _nextTokenId;
     uint256 public maxSupply;
     uint256 public mintPrice;
     string private baseTokenURI;
 
-    constructor(string memory baseURI, uint256 _maxSupply, uint256 _mintPrice) ERC721("LondyNFT", "LNFT") Onwable(msg.sender){
+    constructor(string memory baseURI, uint256 _maxSupply, uint256 _mintPrice) ERC721("LondyNFT", "LNFT") Ownable() {
         baseTokenURI = baseURI;
         maxSupply = _maxSupply;
         mintPrice = _mintPrice;
-
     }
 
-
-    //mint NFT, one at a time
     function mint() external payable {
         require(_nextTokenId <= maxSupply, "No more NFT to mint");
         require(msg.value >= mintPrice, "Insufficient funds!");
+        require(balanceOf(msg.sender) == 0;, "Cannot mint more than one NFT");
 
         uint256 tokenId = _nextTokenId;
-
         _safeMint(msg.sender, tokenId);
         _nextTokenId++;
-
     }
 
     function ownerMint() external onlyOwner {
         require(_nextTokenId <= maxSupply, "No more NFT to mint");
 
         uint256 tokenId = _nextTokenId;
-
         _safeMint(msg.sender, tokenId);
         _nextTokenId++;
     }
 
-    //how many tokens have been minted for far
-    function totalSupply() public view returns(uint256){
+    function totalSupply() public view returns (uint256) {
         return _nextTokenId;
     }
 
-    //assign the base  to our state variable
-    function _baseURI() internal view override returns (string memory){
+    function _baseURI() internal view override returns (string memory) {
         return baseTokenURI;
     }
 
-    //set baseURI to new link
     function setBaseURI(string memory newBaseURI) external onlyOwner {
         baseTokenURI = newBaseURI;
     }
 
-    //owner withdraws funds from selling NFTs
     function withdraw() external onlyOwner {
-        require(address(this).balance == 0, "No funds in contract");
+        require(address(this).balance > 0, "No funds in contract");
         payable(owner()).transfer(address(this).balance);
     }
 
+    //our NFT is SoulBound: that means once you own it, you cannot transfer ownership to someone else.
+    //We need to add that logic in transfer related functions
+    //we want the transaction to revet whenever the owner tries to transfer
+    //the main function that every transfer goes through is _update
+    function _update(address to, uint256 tokenId, address auth) internal override returns (address){
+        //get the owner of the nft
+        address from = _ownerOf(tokenId);
+
+        //we need to only allow transfer if the nft has no owner (address(0))
+        require(from == address(0), "Cannot transfer NFT to another owner");
+
+        return super._update(to, tokenId, auth);
+
+    }
+
+
+    
 }
